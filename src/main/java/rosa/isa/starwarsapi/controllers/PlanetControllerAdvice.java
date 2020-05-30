65bf92e50215f19b1ad4d22aeedbb267bd5acb25
Isabelle Rosa <ibell.pereira@gmail.com>
2020-05-30 15:12:11 -0300

    Handling StarWarsExternalServiceException and adding a general exception handler && Adding missing logs

--- src/main/java/rosa/isa/starwarsapi/controllers/PlanetControllerAdvice.java
+++ src/main/java/rosa/isa/starwarsapi/controllers/PlanetControllerAdvice.java
@@ -1,5 +1,6 @@
 package rosa.isa.starwarsapi.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,22 +8,39 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import rosa.isa.starwarsapi.exceptions.PlanetAlreadyExistsException;
 import rosa.isa.starwarsapi.exceptions.PlanetNotFoundException;
+import rosa.isa.starwarsapi.exceptions.StarWarsExternalServiceException;
 
+@Slf4j
 @ControllerAdvice
 public class PlanetControllerAdvice {
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(PlanetAlreadyExistsException.class)
-    void planetAlreadyExistsExceptionHandler() {
+    void planetAlreadyExistsExceptionHandler(PlanetAlreadyExistsException planetAlreadyExistsException) {
+        log.error("Planet already exists", planetAlreadyExistsException);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public void methodArgumentNotValidExceptionHandler() {
+    public void methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException methodArgumentNotValidException) {
+        log.error("Planet is invalid", methodArgumentNotValidException);
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(PlanetNotFoundException.class)
-    public void planetNotFoundExceptionHandler() {
+    public void planetNotFoundExceptionHandler(PlanetNotFoundException planetNotFoundException) {
+        log.error("Planet not found", planetNotFoundException);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
+    @ExceptionHandler(StarWarsExternalServiceException.class)
+    public void starWarsExternalServiceExceptionHandler(StarWarsExternalServiceException starWarsExternalServiceException) {
+        log.error("An error occurred in Star Wars external service", starWarsExternalServiceException);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public void generalExceptionHandler(Exception exception) {
+        log.error("An unknown error occurred", exception);
     }
 }
